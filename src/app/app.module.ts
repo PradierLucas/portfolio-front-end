776fa21166939fdf224125370558b7163529c707
lucas <lucaspradier73@gmail.com>
2023-01-21 13:22:52 -0300

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaEducacionComponent } from './componentes/experiencia-educacion/experiencia-educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaEducacionComponent,
+    SkillsComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/experiencia-educacion/experiencia-educacion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia-educacion',
+  templateUrl: './experiencia-educacion.component.html',
+  styleUrls: ['./experiencia-educacion.component.css']
+})
+export class ExperienciaEducacionComponent {
+
+}
